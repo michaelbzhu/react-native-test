f4b2c71dff636cb8acbf1fd8e60a900f3aff5dd0
emil <emiltliu0@gmail.com>
2022-08-20 14:05:29 -0500

    login and styles init

--- App.js
+++ App.js
@@ -1,23 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, TouchableOpacity, View, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { ApplicationProvider, Layout, Text, Input, Button } from '@ui-kitten/components';
+import * as eva from '@eva-design/eva';
+import tw from 'twrnc';
 
 export default function App() {
-  const [count, setCount] = useState(0)
-  const onPress = () => {
-    setCount(count + 1)
-  }
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <TouchableOpacity style={styles.button} onPress={onPress}>
-        <Text style={{color: "white",}}>Increment</Text>
-      </TouchableOpacity>
-      <View>
-        <Text>Count: {count}</Text>
-      </View>
-      <StatusBar style="auto" />
-    </View>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={tw`flex-col items-center justify-center h-full w-full`}>
+          <View style={tw`flex-col items-center justify-between h-4/12 w-9/12`}>
+            <Text category='h2'>Join RoamPay</Text>
+            <View style={tw`flex-col items-center justify-around w-full h-6/12`}>
+              <Input label='Email' placeholder='Email' value={email} onChangeText={nextValue => setEmail(nextValue)}></Input>
+              <Input label='Password' placeholder='Password' value={password} onChangeText={nextValue => setPassword(nextValue)}></Input>
+            </View>
+            <StatusBar style="auto" />
+            <Button status='primary' style={tw`w-1/2`}>Sign Up</Button>
+            <View style={tw`flex-row`}>
+              <Text category="s1">Already a Roamer? </Text>
+              <Text style={tw`underline`}>Log In</Text>
+            </View>
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    </ApplicationProvider>
   );
 }
 
@@ -27,6 +38,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    width: "80%",
   },
   button: {
     padding: 10,
--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 3. Navigate to root folder of repo
 4. run `expo start` to generate a qr code
 5. Scan the QR code on your phone which should open a dev version of the app in the expo mobile app
+
+
+Alternative steps:
+npm install @expo/ngrok@4.1.0
+expo-cli start --tunnel  
+
