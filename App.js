a8ba731ddc3311b8b304e9267f5029416b9c8e69
Michael Zhu <michael.zhu@retool.com>
2022-08-20 14:42:21 -0700

    add firebase auth scaffolding

--- App.js
+++ App.js
@@ -1,24 +1,40 @@
-import { Signup } from './components/Signup';
-import { Login } from './components/Login';
-import { Home } from './components/Home';
-import { NativeRouter, Route, Link, Routes } from "react-router-native";
-import { View } from 'react-native';
-import { ApplicationProvider } from '@ui-kitten/components';
-import 'react-native-get-random-values';
-import 'react-native-url-polyfill/auto';
-import * as eva from '@eva-design/eva';
+import { Signup } from "./components/Signup"
+import { Login } from "./components/Login"
+import { Home } from "./components/Home"
+import { NativeRouter, Route, Link, Routes } from "react-router-native"
+import { View } from "react-native"
+import { ApplicationProvider } from "@ui-kitten/components"
+import "react-native-get-random-values"
+import "react-native-url-polyfill/auto"
+import * as eva from "@eva-design/eva"
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBYtvszJhYMn_7UwhIzQ_7lH4gzNQHD8-8",
+  authDomain: "floodgate-squad.firebaseapp.com",
+  projectId: "floodgate-squad",
+  storageBucket: "floodgate-squad.appspot.com",
+  messagingSenderId: "495661629168",
+  appId: "1:495661629168:web:b642b61f5394be9a4c8c35",
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
 
 export default function App() {
-
   return (
     <ApplicationProvider {...eva} theme={eva.light}>
       <NativeRouter>
         <Routes>
-          <Route exact path="/" element={<Home/>} />
-          <Route path="/signup" element={<Signup/>} />
-          <Route path="/login" element={<Login/>} />
+          <Route exact path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </NativeRouter>
     </ApplicationProvider>
-  );
+  )
 }
--- components/Login.js
+++ components/Login.js
@@ -1,30 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { StyleSheet, TouchableOpacity, View, TouchableWithoutFeedback, Keyboard } from 'react-native';
-import { ApplicationProvider, Layout, Text, Input, Button } from '@ui-kitten/components';
-import tw from 'twrnc';
-import { Link } from "react-router-native";
-
+import { StatusBar } from "expo-status-bar"
+import { useState } from "react"
+import { StyleSheet, TouchableOpacity, View, TouchableWithoutFeedback, Keyboard, Alert } from "react-native"
+import { ApplicationProvider, Layout, Text, Input, Button } from "@ui-kitten/components"
+import tw from "twrnc"
+import { Link } from "react-router-native"
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
 
 export function Login({ navigation }) {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
 
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const notValidEmail = (maybeValidEmail) => maybeValidEmail === ""
+  const notValidPassword = (maybeValidPassword) => maybeValidPassword === ""
+
+  const auth = getAuth()
+
+  const handleLogIn = () => {
+    if (notValidEmail(email)) {
+      Alert.alert("Invalid Email")
+      return
+    }
+    if (notValidPassword(password)) {
+      Alert.alert("Invalid Password")
+      return
+    }
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user
+        console.log({ userCredential, user })
+        Alert.alert(`logged in as ${user.email} with uid: ${user.uid}`)
+      })
+      .catch((error) => {
+        const errorCode = error.code
+        const errorMessage = error.message
+        console.log({ errorCode, errorMessage })
+      })
+  }
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <View style={tw`flex-col items-center justify-center h-full w-full`}>
         <View style={tw`flex-col items-center justify-between h-4/12 w-9/12`}>
-          <Text category='h2'>Welcome Back</Text>
+          <Text category="h2">Welcome Back</Text>
           <View style={tw`flex-col items-center justify-around w-full h-6/12`}>
-            <Input label='Email' placeholder='Email' value={email} onChangeText={nextValue => setEmail(nextValue)}></Input>
-            <Input label='Password' placeholder='Password' value={password} onChangeText={nextValue => setPassword(nextValue)}></Input>
+            <Input
+              label="Email"
+              placeholder="Email"
+              value={email}
+              onChangeText={(nextValue) => setEmail(nextValue)}
+            ></Input>
+            <Input
+              label="Password"
+              placeholder="Password"
+              value={password}
+              onChangeText={(nextValue) => setPassword(nextValue)}
+            ></Input>
           </View>
           <StatusBar style="auto" />
-          <Button status='primary' style={tw`w-1/2`}>Log In</Button>
+          <Button status="primary" style={tw`w-1/2`} onPress={handleLogIn}>
+            Log In
+          </Button>
           <View style={tw`flex-row`}>
             <Text category="s1">Don't have an account? </Text>
-            <Link to="/"><Text style={tw`underline`} >Sign Up</Text></Link>
+            <Link to="/">
+              <Text style={tw`underline`}>Sign Up</Text>
+            </Link>
           </View>
         </View>
       </View>
