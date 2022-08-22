21cc3f18f09bc230fa420b215c5a448fbdbe0d3f
Michael Zhu <michael.zhu@retool.com>
2022-08-21 17:37:55 -0700

    add a testing option to usePublicKey

--- hooks/usePublicKey.js
+++ hooks/usePublicKey.js
@@ -1,23 +1,25 @@
 import { useEffect, useState } from "react"
 import { getAuth } from "firebase/auth"
 import { getFirestore, doc, getDoc } from "firebase/firestore"
-import { createAssociatedTokenAccount, getOrCreateAssociatedTokenAccount, getAssociatedTokenAddress } from '@solana/spl-token';
-import { Cluster, clusterApiUrl, Connection, PublicKey, Keypair, Transaction, sendAndConfirmTransaction } from '@solana/web3.js';
+import { getAssociatedTokenAddress } from "@solana/spl-token"
+import { Connection, PublicKey } from "@solana/web3.js"
 
-
-export const usePublicKey = () => {
+export const usePublicKey = (useTestKey = false) => {
   const [publicKey, setPublicKey] = useState(null)
   const USDC_MINT_ADDR = "6L61933r4BBMJwoejjCZeJtDWouTtgvVAokDiSqyt4DQ"
-  const connection = new Connection("https://api.devnet.solana.com", 'processed');
-  const [userBalance, setUserBalance] = useState(null);
-
-
+  const connection = new Connection("https://api.devnet.solana.com", "processed")
+  const [userBalance, setUserBalance] = useState(null)
 
   const db = getFirestore()
   const auth = getAuth()
   const userUID = auth.currentUser?.uid
 
   async function getPublicKeyOfCurrentUser() {
+    if (useTestKey) {
+      setPublicKey(new PublicKey("9sgcecPQ1dNvfHZwiAhEuZKw8AJVyvab5AthHt3vBAuw"))
+      getTokenBalance(new PublicKey("9sgcecPQ1dNvfHZwiAhEuZKw8AJVyvab5AthHt3vBAuw"))
+      return
+    }
     if (!userUID) {
       console.error("Can't get user UID")
       return
@@ -40,13 +42,13 @@ export const usePublicKey = () => {
   }
 
   const getTokenBalance = async (pk) => {
-    if(pk){
-      console.log('publicKey', pk)
-      const addr = await getAssociatedTokenAddress(new PublicKey(USDC_MINT_ADDR), pk);
-      const balance = await connection.getTokenAccountBalance(addr);
+    if (pk) {
+      console.log("publicKey", pk)
+      const addr = await getAssociatedTokenAddress(new PublicKey(USDC_MINT_ADDR), pk)
+      const balance = await connection.getTokenAccountBalance(addr)
 
-      console.log('addr', addr, "balance", balance.value.uiAmount)
-      setUserBalance(balance.value.uiAmount);
+      console.log("addr", addr, "balance", balance.value.uiAmount)
+      setUserBalance(balance.value.uiAmount)
     }
   }
 
